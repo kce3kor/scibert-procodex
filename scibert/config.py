cc95cae986a80fba15eda93349a3d9e9fa25753b
kce3kor <kamal.shrestha@in.bosch.com>
2023-09-06 16:25:42 +0530

    lightning ai - unit 5 is complete

--- scibert/config.py
+++ scibert/config.py
@@ -6,7 +6,7 @@ CKPTH_DIR = "checkpoints/"
 DATA_DIR = "data/raw"
 DATA = "data/raw/20230821_full_data.pkl"
 TEST_DIR = "data/raw/test_ids.xlsx"
-
+PROCESSED_DATA = "data/processed/generated_inputs.pkl"
 LOG_DIR = BASE_DIR + "/logs"
 
 LOG_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> >> <level>{message}</level>"
@@ -17,7 +17,7 @@ LABEL_MAPPER = {"Relevant": 1, "Not relevant": 0}
 
 MODEL = "tinyBERT"
 
-SEED = 0
+SEED = 123
 BATCH_SIZE = 8
 EPOCHS = 10
 TOKENS_MAX_LENGTH = 512
